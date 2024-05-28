105c07412e7c796c96a5d025f072a56b0b90e114
Allen Cheung <94336345+AllenCheung0213@users.noreply.github.com>
2024-05-27 21:49:07 -0400

    Adding to the project
    Git ignore and fixing issues with poetry

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from .oauth import configure_oauth
 from .routes import main as main_blueprint
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='../templates')
+    print("Template folder:", app.template_folder)
     app.config.from_object(Config)
     
     configure_oauth(app)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
+    print("Current working directory:", os.getcwd())
     app.run(host='0.0.0.0', port=3000)
